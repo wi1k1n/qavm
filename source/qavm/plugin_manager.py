73909d85238f30df69235e0471d582f72ae76fe0
i_mazlov <i_mazlov@maxon.net>
2024-06-19 16:47:55 +0200

    plugin ID and version validation

--- source/qavm/plugin_manager.py
+++ source/qavm/plugin_manager.py
@@ -1,4 +1,4 @@
-import importlib.util, os
+import importlib.util, os, re
 
 import logs
 logger = logs.logger
@@ -24,17 +24,31 @@ class Plugin:
             spec = importlib.util.spec_from_file_location(self.pluginName, self.pluginMainFile)
             plugin = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(plugin)
+
             pluginID = getattr(plugin, 'PLUGIN_ID', '')
-            if not pluginID:
+            if not Plugin.ValidatePluginId(pluginID):
                 raise Exception(f'PLUGIN_ID not found for: {self.pluginMainFile}')
+            
             pluginVersion = getattr(plugin, 'PLUGIN_VERSION', '')
-            if not pluginVersion:
+            if not Plugin.ValidatePluginVersion(pluginVersion):
                 raise Exception(f'PLUGIN_VERSION not found for: {self.pluginMainFile}')
             
             logger.info(f'Loaded plugin: {self.pluginName} @ {pluginVersion} ({pluginID})')
             self.pluginModule = plugin
         except:
             logger.exception(f'Failed to load plugin: {self.pluginMainFile}')
+    
+    @staticmethod
+    def ValidatePluginId(pluginId: str) -> bool:
+        # checks plugin id to be in domain-style format
+        pattern = re.compile("(?:[a-z0-9](?:[a-z0-9]{0,61}[a-z0-9])?\\.)+[a-z0-9][a-z0-9]{0,61}[a-z0-9]")
+        return pattern.match(pluginId) is not None
+    
+    @staticmethod
+    def ValidatePluginVersion(pluginVersion: str) -> bool:
+        # plugin version should be in XXX.XXX.XXXX format, where each part can be at least 1 digit long
+        pattern = re.compile("(?:[0-9]{1,3}\.){2}[0-9]{1,4}")
+        return pattern.match(pluginVersion) is not None
 
 class PluginManager:
     def __init__(self, pluginsFolderPath: str) -> None:
