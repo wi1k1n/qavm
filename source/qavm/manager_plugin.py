fd52882e32164f78b251ade916197d2938ef66aa
Ilya Mazlov <mazlov.i.a@gmail.com>
2024-07-25 22:42:32 +0200

    spaces -> tabs;

--- source/qavm/manager_plugin.py
+++ source/qavm/manager_plugin.py
@@ -7,172 +7,172 @@ import logs
 logger = logs.logger
 
 class Module:
-    def __init__(self, plugin, regData):
-        self.id = regData.get('id', None)
+	def __init__(self, plugin, regData):
+		self.id = regData.get('id', None)
 
 class SoftwareHandler(Module):
-    def __init__(self, plugin, regData) -> None:
-        super().__init__(plugin, regData)
-        
-        if not Plugin.ValidateID(self.id):
-            raise Exception(f'Invalid or missing Software ID: {self.id}')
-        
-        self.name = regData.get('name', self.id)
-        
-        self.qualifierClass = regData.get('qualifier', None)
-        if not self.qualifierClass or not issubclass(self.qualifierClass, BaseQualifier):  # required
-            raise Exception(f'Missing or invalid qualifier for software: {self.id}')
-        self.descriptorClass = regData.get('descriptor', None)
-        if not self.descriptorClass or not issubclass(self.descriptorClass, BaseDescriptor):  # required
-            raise Exception(f'Missing or invalid descriptor for software: {self.id}')
-        self.tileBuilderClasses = regData.get('tile_builders', {})
-        if not self.tileBuilderClasses or not isinstance(self.tileBuilderClasses, dict) or '' not in self.tileBuilderClasses \
-            or not all([f for f in self.tileBuilderClasses.values() if issubclass(f, BaseTileBuilder)]):  # required
-            raise Exception(f'Missing or invalid tile builder for software: {self.id}')
-        
-        self.settingsClass = regData.get('settings', None)  # optional
-    
-    def GetName(self) -> str:
-        return self.name
-    
-    def GetQualifierClass(self) -> BaseQualifier.__class__:
-        return self.qualifierClass
-    def GetDescriptorClass(self) -> BaseDescriptor.__class__:
-        return self.descriptorClass
-    def GetTileBuilderClass(self, context='') -> BaseTileBuilder.__class__:
-        return self.tileBuilderClasses.get(context, self.tileBuilderClasses.get('', None))
-    def GetSettingsClass(self) -> BaseSettings.__class__:
-        return self.settingsClass
+	def __init__(self, plugin, regData) -> None:
+		super().__init__(plugin, regData)
+		
+		if not Plugin.ValidateID(self.id):
+			raise Exception(f'Invalid or missing Software ID: {self.id}')
+		
+		self.name = regData.get('name', self.id)
+		
+		self.qualifierClass = regData.get('qualifier', None)
+		if not self.qualifierClass or not issubclass(self.qualifierClass, BaseQualifier):  # required
+			raise Exception(f'Missing or invalid qualifier for software: {self.id}')
+		self.descriptorClass = regData.get('descriptor', None)
+		if not self.descriptorClass or not issubclass(self.descriptorClass, BaseDescriptor):  # required
+			raise Exception(f'Missing or invalid descriptor for software: {self.id}')
+		self.tileBuilderClasses = regData.get('tile_builders', {})
+		if not self.tileBuilderClasses or not isinstance(self.tileBuilderClasses, dict) or '' not in self.tileBuilderClasses \
+			or not all([f for f in self.tileBuilderClasses.values() if issubclass(f, BaseTileBuilder)]):  # required
+			raise Exception(f'Missing or invalid tile builder for software: {self.id}')
+		
+		self.settingsClass = regData.get('settings', None)  # optional
+	
+	def GetName(self) -> str:
+		return self.name
+	
+	def GetQualifierClass(self) -> BaseQualifier.__class__:
+		return self.qualifierClass
+	def GetDescriptorClass(self) -> BaseDescriptor.__class__:
+		return self.descriptorClass
+	def GetTileBuilderClass(self, context='') -> BaseTileBuilder.__class__:
+		return self.tileBuilderClasses.get(context, self.tileBuilderClasses.get('', None))
+	def GetSettingsClass(self) -> BaseSettings.__class__:
+		return self.settingsClass
 
 class Plugin:
-    def __init__(self, pluginModule: object) -> None:
-        self.module = pluginModule
+	def __init__(self, pluginModule: object) -> None:
+		self.module = pluginModule
 
-        self.pluginID = ''
-        self.pluginVersion = ''
-        self.pluginName = self.module.__name__
+		self.pluginID = ''
+		self.pluginVersion = ''
+		self.pluginName = self.module.__name__
 
-        self.softwareHandlers: dict[str, SoftwareHandler] = dict()  # softwareID: SoftwareHandler
+		self.softwareHandlers: dict[str, SoftwareHandler] = dict()  # softwareID: SoftwareHandler
 
-        # First check if plugin contains PLUGIN_ID and PLUGIN_VERSION
-        self.pluginID = getattr(self.module, 'PLUGIN_ID', '')
-        if not Plugin.ValidateUID(self.pluginID):
-            raise Exception(f'Invalid or missing PLUGIN_ID for: {self.module.__name__}')
-        
-        self.pluginVersion = getattr(self.module, 'PLUGIN_VERSION', '')
-        if not Plugin.ValidateVersion(self.pluginVersion):
-            raise Exception(f'Invalid or missing PLUGIN_VERSION for: {self.module.__name__}')
+		# First check if plugin contains PLUGIN_ID and PLUGIN_VERSION
+		self.pluginID = getattr(self.module, 'PLUGIN_ID', '')
+		if not Plugin.ValidateUID(self.pluginID):
+			raise Exception(f'Invalid or missing PLUGIN_ID for: {self.module.__name__}')
+		
+		self.pluginVersion = getattr(self.module, 'PLUGIN_VERSION', '')
+		if not Plugin.ValidateVersion(self.pluginVersion):
+			raise Exception(f'Invalid or missing PLUGIN_VERSION for: {self.module.__name__}')
 
-        self.LoadModuleSoftware()
-    
-    def LoadModuleSoftware(self) -> None:
-        pluginSoftwareRegisterFunc = getattr(self.module, 'RegisterModuleSoftware', None)
-        if pluginSoftwareRegisterFunc is None:
-            return
-        
-        softwareRegDataList = pluginSoftwareRegisterFunc()
+		self.LoadModuleSoftware()
+	
+	def LoadModuleSoftware(self) -> None:
+		pluginSoftwareRegisterFunc = getattr(self.module, 'RegisterModuleSoftware', None)
+		if pluginSoftwareRegisterFunc is None:
+			return
+		
+		softwareRegDataList = pluginSoftwareRegisterFunc()
 
-        for softwareRegData in softwareRegDataList:
-            softwareHandler = SoftwareHandler(self, softwareRegData)
+		for softwareRegData in softwareRegDataList:
+			softwareHandler = SoftwareHandler(self, softwareRegData)
 
-            if softwareHandler.id in self.softwareHandlers:
-                raise Exception(f'Duplicate software ID found: {self.id}')
-            
-            self.softwareHandlers[softwareHandler.id] = softwareHandler
+			if softwareHandler.id in self.softwareHandlers:
+				raise Exception(f'Duplicate software ID found: {self.id}')
+			
+			self.softwareHandlers[softwareHandler.id] = softwareHandler
 
-    def GetUID(self) -> str:
-        return self.pluginID
+	def GetUID(self) -> str:
+		return self.pluginID
 
-    def GetName(self) -> str:
-        return self.pluginName
-    
-    def GetVersionStr(self) -> str:
-        return self.pluginVersion
-    
-    def GetVersion(self) -> tuple[int, int, int]:
-        return tuple(map(int, self.pluginVersion.split('.')))
-    
-    def GetSoftwareHandlers(self) -> dict[str, SoftwareHandler]:
-        return self.softwareHandlers
-            
-    
-    
-    @staticmethod
-    def ValidateUID(UID: str) -> bool:
-        # checks id to be in domain-style format
-        pattern = re.compile("(?:[a-z0-9](?:[a-z0-9]{0,61}[a-z0-9])?\\.)+[a-z0-9][a-z0-9]{0,61}[a-z0-9]")
-        return pattern.match(UID) is not None
-    
-    @staticmethod
-    def ValidateID(ID: str) -> bool:
-        # checks id to be in domain-style format + single alphanumrical words
-        pattern = re.compile("(?:[a-z0-9](?:[a-z0-9]{0,61}[a-z0-9]\\.)?)+[a-z0-9][a-z0-9]{0,61}[a-z0-9]")
-        return pattern.match(ID) is not None
-    
-    @staticmethod
-    def ValidateVersion(version: str) -> bool:
-        # version should be in XXX.XXX.XXXX format, where each part can be at least 1 digit long
-        pattern = re.compile("(?:[0-9]{1,3}\.){2}[0-9]{1,4}")
-        return pattern.match(version) is not None
+	def GetName(self) -> str:
+		return self.pluginName
+	
+	def GetVersionStr(self) -> str:
+		return self.pluginVersion
+	
+	def GetVersion(self) -> tuple[int, int, int]:
+		return tuple(map(int, self.pluginVersion.split('.')))
+	
+	def GetSoftwareHandlers(self) -> dict[str, SoftwareHandler]:
+		return self.softwareHandlers
+			
+	
+	
+	@staticmethod
+	def ValidateUID(UID: str) -> bool:
+		# checks id to be in domain-style format
+		pattern = re.compile("(?:[a-z0-9](?:[a-z0-9]{0,61}[a-z0-9])?\\.)+[a-z0-9][a-z0-9]{0,61}[a-z0-9]")
+		return pattern.match(UID) is not None
+	
+	@staticmethod
+	def ValidateID(ID: str) -> bool:
+		# checks id to be in domain-style format + single alphanumrical words
+		pattern = re.compile("(?:[a-z0-9](?:[a-z0-9]{0,61}[a-z0-9]\\.)?)+[a-z0-9][a-z0-9]{0,61}[a-z0-9]")
+		return pattern.match(ID) is not None
+	
+	@staticmethod
+	def ValidateVersion(version: str) -> bool:
+		# version should be in XXX.XXX.XXXX format, where each part can be at least 1 digit long
+		pattern = re.compile("(?:[0-9]{1,3}\.){2}[0-9]{1,4}")
+		return pattern.match(version) is not None
 
 
 class PluginManager:
-    def __init__(self, app, pluginsFolderPath: Path) -> None:
-        self.app = app
-        self.pluginsFolderPath: Path = pluginsFolderPath
-        self.plugins: dict[str, Plugin] = dict()
+	def __init__(self, app, pluginsFolderPath: Path) -> None:
+		self.app = app
+		self.pluginsFolderPath: Path = pluginsFolderPath
+		self.plugins: dict[str, Plugin] = dict()
 
-        if not self.pluginsFolderPath.exists():
-            self.pluginsFolderPath.mkdir(parents=True)
-            logger.info(f'Created plugins folder: {self.pluginsFolderPath}')
+		if not self.pluginsFolderPath.exists():
+			self.pluginsFolderPath.mkdir(parents=True)
+			logger.info(f'Created plugins folder: {self.pluginsFolderPath}')
 
-    def LoadPlugins(self) -> bool:
-        if not self.pluginsFolderPath.exists():
-            logger.error(f'Plugins folder not found: {self.pluginsFolderPath}')
-            return False
-        
-        # Iterate over plugins
-        for pluginFolderPath in self.pluginsFolderPath.iterdir():
-            if not pluginFolderPath.is_dir():
-                continue
-            
-            pluginName = pluginFolderPath.name
-            pluginMainFile = pluginFolderPath/f'{pluginName}.py'
+	def LoadPlugins(self) -> bool:
+		if not self.pluginsFolderPath.exists():
+			logger.error(f'Plugins folder not found: {self.pluginsFolderPath}')
+			return False
+		
+		# Iterate over plugins
+		for pluginFolderPath in self.pluginsFolderPath.iterdir():
+			if not pluginFolderPath.is_dir():
+				continue
+			
+			pluginName = pluginFolderPath.name
+			pluginMainFile = pluginFolderPath/f'{pluginName}.py'
 
-            if not pluginMainFile.exists():
-                logger.error(f'Plugin main file not found: {pluginMainFile}')
-                return
+			if not pluginMainFile.exists():
+				logger.error(f'Plugin main file not found: {pluginMainFile}')
+				return
 
-            try:
-                spec = importlib.util.spec_from_file_location(pluginName, pluginMainFile)
-                pluginPyModule = importlib.util.module_from_spec(spec)
-                spec.loader.exec_module(pluginPyModule)
-                
-                plugin = Plugin(pluginPyModule)
-                logger.info(f'Loaded plugin: {pluginName} @ {plugin.GetVersionStr()} ({plugin.GetUID()})')
-                self.plugins[plugin.pluginID] = plugin
-            
-            except:
-                logger.exception(f'Failed to load plugin: {pluginMainFile}')
-    
-    def GetPlugins(self) -> list[Plugin]:
-        return list(self.plugins.values())  # TODO: rewrite with yield
-    
-    def GetPlugin(self, pluginUID: str) -> Plugin:
-        return self.plugins.get(pluginUID, None)
-    
-    def GetSoftwareHandlers(self) -> list[tuple[str, str, SoftwareHandler]]:  # TODO: rewrite with yield
-        result: list[tuple[str, str, SoftwareHandler]] = []  # [pluginID, softwareID, SoftwareHandler]
-        for plugin in self.plugins.values():
-            for softwareID, softwareHandler in plugin.GetSoftwareHandlers().items():
-                result.append((plugin.pluginID, softwareID, softwareHandler))
-        return result
-    
-    def GetSoftwareHandler(self, softwareUID: str) -> SoftwareHandler:
-        if '#' not in softwareUID:
-            return None
-        pluginUID, softwareID = softwareUID.split('#')
-        plugin = self.GetPlugin(pluginUID)
-        if not plugin:
-            return None
-        return plugin.GetSoftwareHandlers().get(softwareID, None)
+			try:
+				spec = importlib.util.spec_from_file_location(pluginName, pluginMainFile)
+				pluginPyModule = importlib.util.module_from_spec(spec)
+				spec.loader.exec_module(pluginPyModule)
+				
+				plugin = Plugin(pluginPyModule)
+				logger.info(f'Loaded plugin: {pluginName} @ {plugin.GetVersionStr()} ({plugin.GetUID()})')
+				self.plugins[plugin.pluginID] = plugin
+			
+			except:
+				logger.exception(f'Failed to load plugin: {pluginMainFile}')
+	
+	def GetPlugins(self) -> list[Plugin]:
+		return list(self.plugins.values())  # TODO: rewrite with yield
+	
+	def GetPlugin(self, pluginUID: str) -> Plugin:
+		return self.plugins.get(pluginUID, None)
+	
+	def GetSoftwareHandlers(self) -> list[tuple[str, str, SoftwareHandler]]:  # TODO: rewrite with yield
+		result: list[tuple[str, str, SoftwareHandler]] = []  # [pluginID, softwareID, SoftwareHandler]
+		for plugin in self.plugins.values():
+			for softwareID, softwareHandler in plugin.GetSoftwareHandlers().items():
+				result.append((plugin.pluginID, softwareID, softwareHandler))
+		return result
+	
+	def GetSoftwareHandler(self, softwareUID: str) -> SoftwareHandler:
+		if '#' not in softwareUID:
+			return None
+		pluginUID, softwareID = softwareUID.split('#')
+		plugin = self.GetPlugin(pluginUID)
+		if not plugin:
+			return None
+		return plugin.GetSoftwareHandlers().get(softwareID, None)
--- tools/deploy_plugins.py
+++ tools/deploy_plugins.py
@@ -4,97 +4,97 @@ import importlib.util
 IGNORE_DIRS_LIST = set(['__pycache__'])
 
 def file_hash(file_path):
-    """Compute the hash of a file."""
-    hash_algo = hashlib.sha256()
-    with open(file_path, 'rb') as f:
-        while chunk := f.read(8192):
-            hash_algo.update(chunk)
-    return hash_algo.hexdigest()
+	"""Compute the hash of a file."""
+	hash_algo = hashlib.sha256()
+	with open(file_path, 'rb') as f:
+		while chunk := f.read(8192):
+			hash_algo.update(chunk)
+	return hash_algo.hexdigest()
 
 # TODO: using hardlink is potentially easier
 def DeployFolder(sourceFolderPath: str, targetFolderPath: str) -> None:
-    sourceFolderPath = os.path.abspath(sourceFolderPath)
-    targetFolderPath = os.path.abspath(targetFolderPath)
+	sourceFolderPath = os.path.abspath(sourceFolderPath)
+	targetFolderPath = os.path.abspath(targetFolderPath)
 
-    if not os.path.isdir(sourceFolderPath):
-        raise ValueError(f"Source folder '{sourceFolderPath}' does not exist or is not a directory.")
-    
-    if not os.path.exists(targetFolderPath):
-        os.makedirs(targetFolderPath)
-        print(f'> Created plugin folder: {targetFolderPath}')
-    
-    pluginFiles: set = set()
+	if not os.path.isdir(sourceFolderPath):
+		raise ValueError(f"Source folder '{sourceFolderPath}' does not exist or is not a directory.")
+	
+	if not os.path.exists(targetFolderPath):
+		os.makedirs(targetFolderPath)
+		print(f'> Created plugin folder: {targetFolderPath}')
+	
+	pluginFiles: set = set()
 
-    # Iterate through all files in the source folder
-    for root, dirs, files in os.walk(sourceFolderPath):
-        relative_path = os.path.relpath(root, sourceFolderPath)
-        target_root = os.path.join(targetFolderPath, relative_path)
+	# Iterate through all files in the source folder
+	for root, dirs, files in os.walk(sourceFolderPath):
+		relative_path = os.path.relpath(root, sourceFolderPath)
+		target_root = os.path.join(targetFolderPath, relative_path)
 
-        if not os.path.exists(target_root):
-            os.makedirs(target_root)
-            print(f'> Created target folder: {target_root}')
+		if not os.path.exists(target_root):
+			os.makedirs(target_root)
+			print(f'> Created target folder: {target_root}')
 
-        for file in files:
-            source_file_path = os.path.join(root, file)
-            target_file_path = os.path.join(target_root, file)
+		for file in files:
+			source_file_path = os.path.join(root, file)
+			target_file_path = os.path.join(target_root, file)
 
-            print(f'>>> {source_file_path} -> {target_file_path}')
-            pluginFiles.add(os.path.abspath(target_file_path))
+			print(f'>>> {source_file_path} -> {target_file_path}')
+			pluginFiles.add(os.path.abspath(target_file_path))
 
-            result = ''
-            if os.path.exists(target_file_path):
-                source_hash = file_hash(source_file_path)
-                target_hash = file_hash(target_file_path)
-                
-                if source_hash != target_hash:
-                    shutil.copy2(source_file_path, target_file_path)
-                    result = 'UPDATED'
-                else:
-                    result = 'SKIPPED'
-            else:
-                shutil.copy2(source_file_path, target_file_path)
-                result = 'COPIED'
-            print(f'\t{result}')
-    
-    # Double check if there are any files in the target folder that are not in the source folder
-    leftoverFiles = set()
-    global IGNORE_DIRS_LIST
-    for root, dirs, files in os.walk(targetFolderPath):
-        # TODO: this line below accounts for paths in IGNORE_DIRS_LIST relative to plugins folder (e.g. inner folders with same names would still trigger exception)
-        dirs[:] = [d for d in dirs if os.path.relpath(os.path.join(root, d), targetFolderPath) not in IGNORE_DIRS_LIST]
-        for file in files:
-            target_file_path = os.path.join(root, file)
-            if os.path.abspath(target_file_path) not in pluginFiles:
-                leftoverFiles.add(target_file_path)
-    if len(leftoverFiles):
-        print(f'> Leftover files found:')
-        for file in leftoverFiles:
-            path, fileName = os.path.split(file)
-            print(f'\t{fileName} in {path}')
-        raise ValueError(f'Leftover files found in target folder')
+			result = ''
+			if os.path.exists(target_file_path):
+				source_hash = file_hash(source_file_path)
+				target_hash = file_hash(target_file_path)
+				
+				if source_hash != target_hash:
+					shutil.copy2(source_file_path, target_file_path)
+					result = 'UPDATED'
+				else:
+					result = 'SKIPPED'
+			else:
+				shutil.copy2(source_file_path, target_file_path)
+				result = 'COPIED'
+			print(f'\t{result}')
+	
+	# Double check if there are any files in the target folder that are not in the source folder
+	leftoverFiles = set()
+	global IGNORE_DIRS_LIST
+	for root, dirs, files in os.walk(targetFolderPath):
+		# TODO: this line below accounts for paths in IGNORE_DIRS_LIST relative to plugins folder (e.g. inner folders with same names would still trigger exception)
+		dirs[:] = [d for d in dirs if os.path.relpath(os.path.join(root, d), targetFolderPath) not in IGNORE_DIRS_LIST]
+		for file in files:
+			target_file_path = os.path.join(root, file)
+			if os.path.abspath(target_file_path) not in pluginFiles:
+				leftoverFiles.add(target_file_path)
+	if len(leftoverFiles):
+		print(f'> Leftover files found:')
+		for file in leftoverFiles:
+			path, fileName = os.path.split(file)
+			print(f'\t{fileName} in {path}')
+		raise ValueError(f'Leftover files found in target folder')
 
 if __name__ == "__main__":
-    print('deploy_plugins.py - START')
+	print('deploy_plugins.py - START')
 
-    sourcePluginsFolderPath = os.path.join('source', 'plugins')
+	sourcePluginsFolderPath = os.path.join('source', 'plugins')
 
-    spec = importlib.util.spec_from_file_location('qavm_utils', os.path.join('source', 'qavm', 'qavmapi', 'utils.py'))
-    qavm_utils = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(qavm_utils)
-    if not hasattr(qavm_utils, 'GetPluginsFolderPath'):
-        print('Failed to load source/utils.py')
-        exit(1)
-    targetPluginsFolderPath = qavm_utils.GetPluginsFolderPath()
-    print(f'> Deploying plugins from {sourcePluginsFolderPath} to {targetPluginsFolderPath}')    
+	spec = importlib.util.spec_from_file_location('qavm_utils', os.path.join('source', 'qavm', 'qavmapi', 'utils.py'))
+	qavm_utils = importlib.util.module_from_spec(spec)
+	spec.loader.exec_module(qavm_utils)
+	if not hasattr(qavm_utils, 'GetPluginsFolderPath'):
+		print('Failed to load source/utils.py')
+		exit(1)
+	targetPluginsFolderPath = qavm_utils.GetPluginsFolderPath()
+	print(f'> Deploying plugins from {sourcePluginsFolderPath} to {targetPluginsFolderPath}')    
 
-    for dir in os.scandir(sourcePluginsFolderPath):
-        if not dir.is_dir():
-            continue
-        sourcePluginFolderPath = dir.path
-        targetPluginFolderPath = os.path.join(targetPluginsFolderPath, dir.name)
-        print(f'> Deploying "{dir.name}"')
-        print(f'\t{sourcePluginFolderPath} -> {targetPluginFolderPath}')
+	for dir in os.scandir(sourcePluginsFolderPath):
+		if not dir.is_dir():
+			continue
+		sourcePluginFolderPath = dir.path
+		targetPluginFolderPath = os.path.join(targetPluginsFolderPath, dir.name)
+		print(f'> Deploying "{dir.name}"')
+		print(f'\t{sourcePluginFolderPath} -> {targetPluginFolderPath}')
 
-        DeployFolder(sourcePluginFolderPath, targetPluginFolderPath)
-    
-    print('deploy_plugins.py - FINISHED')
+		DeployFolder(sourcePluginFolderPath, targetPluginFolderPath)
+	
+	print('deploy_plugins.py - FINISHED')
