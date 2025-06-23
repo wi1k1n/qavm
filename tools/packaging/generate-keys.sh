0952bf34f6ef38a4802d5f192ae6bddbcc2f3939
Ilia Mazlov <i_mazlov@maxon.net>
2025-06-23 17:16:43 +0200

    verification key to generated subfolder

--- tools/packaging/generate-keys.sh
+++ tools/packaging/generate-keys.sh
@@ -3,7 +3,7 @@
 source ../../venv/bin/activate
 
 # Generate signing keys
-python signing/generate_keys.py --private signing/keys/private.pem --public signing/keys/public.pem --deployPath ../../source/qavm/verification_key.py
+python signing/generate_keys.py --private signing/keys/private.pem --public signing/keys/public.pem --deployPath ../../source/qavm/generated/verification_key.py
 if [ $? -ne 0 ]; then
     exit 1
 fi
