1367f382cbc586965ae829b747a9e4eef9fd9f50
Ilia Mazlov <i_mazlov@maxon.net>
2025-07-07 18:16:48 +0200

    separate about window

--- source/qavm/window_main.py
+++ source/qavm/window_main.py
@@ -14,9 +14,11 @@ from PyQt6.QtWidgets import (
 
 from qavm.manager_plugin import PluginManager, SoftwareHandler, UID, QAVMWorkspace
 from qavm.manager_settings import SettingsManager, QAVMGlobalSettings
-from qavm.window_note_editor import NoteEditorDialog
 from qavm.manager_descriptor_data import DescriptorDataManager, DescriptorData
 from qavm.manager_tags import TagsManager, Tag
+
+from qavm.window_note_editor import NoteEditorDialog
+from qavm.window_about import AboutDialog
 
 from qavm.qavmapi import (
 	BaseDescriptor, BaseSettings, BaseTileBuilder, BaseTableBuilder,
@@ -645,72 +647,5 @@ class MainWindow(QMainWindow):
 	# 	self.UpdateTilesWidget()
 
 	def _showAboutDialog(self):
-		aboutDialog = QDialog(self)
-		aboutDialog.setWindowTitle("About QAVM")
-		aboutDialog.setMinimumSize(600, 400)
-		aboutDialog.setModal(True)
-
-		mainLayout = QVBoxLayout(aboutDialog)
-
-		# === Top section: Icon + version info ===
-		topLayout = QHBoxLayout()
-
-		# App icon
-		icon = QIcon("res/qavm_icon.png")  # Replace with your icon path or Qt resource
-		pixmap = icon.pixmap(64, 64)
-		iconLabel = QLabel()
-		iconLabel.setPixmap(pixmap)
-		iconLabel.setAlignment(Qt.AlignmentFlag.AlignTop)
-		topLayout.addWidget(iconLabel)
-
-		# Version info
-		versionInfo = (
-			f"<b>QAVM {GetQAVMVersionVariant()}</b><br>"
-			f"Package: {GetPackageVersion()}<br>"
-			f"Build: {GetBuildVersion()}<br><br>"
-		)
-		versionLabel = QLabel(versionInfo)
-		versionLabel.setTextInteractionFlags(Qt.TextInteractionFlag.TextBrowserInteraction)
-		versionLabel.setTextFormat(Qt.TextFormat.RichText)
-		versionLabel.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
-		topLayout.addWidget(versionLabel)
-
-		topLayout.addStretch()
-		mainLayout.addLayout(topLayout)
-
-		# === Scrollable plugin section ===
-		scrollArea = QScrollArea()
-		scrollArea.setWidgetResizable(True)
-		pluginContainer = QWidget()
-		pluginLayout = QVBoxLayout(pluginContainer)
-
-		for plugin in self.pluginManager.GetPlugins():
-			pluginVersion: str = plugin.GetVersionStr()
-			pluginVariant: str = plugin.GetPluginVariant()
-			if pluginVariant:
-				pluginVersion += f" ({pluginVariant})"
-			pluginText = (
-				f"<b>Plugin:</b> {plugin.GetName()}"
-				f"<br><b>Version:</b> {pluginVersion}"
-				f"<br><b>UID:</b> {plugin.GetUID()}"
-				f"<br><b>Executable:</b> <code>{plugin.GetExecutablePath()}</code>"
-				f"<br><b>Developer:</b> {plugin.GetPluginDeveloper()}"
-				f"<br><b>Website:</b> <a href='{plugin.GetPluginWebsite()}'>{plugin.GetPluginWebsite()}</a>"
-			)
-			pluginLabel = QLabel()
-			pluginLabel.setTextFormat(Qt.TextFormat.RichText)
-			pluginLabel.setTextInteractionFlags(Qt.TextInteractionFlag.TextBrowserInteraction)
-			pluginLabel.setOpenExternalLinks(True)
-			pluginLabel.setWordWrap(True)
-			pluginLabel.setText(pluginText)
-			pluginLayout.addWidget(pluginLabel)
-
-		scrollArea.setWidget(pluginContainer)
-		mainLayout.addWidget(scrollArea)
-
-		# === OK Button ===
-		buttonBox = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok)
-		buttonBox.accepted.connect(aboutDialog.accept)
-		mainLayout.addWidget(buttonBox)
-
+		aboutDialog: AboutDialog = AboutDialog(self, self.pluginManager)
 		aboutDialog.exec()
